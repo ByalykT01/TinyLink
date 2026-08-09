f512531e11b758bfc62ea938d908abcec561d691
tbyalyk <byalykt@hotmail.com>
2026-08-09 17:50:59 +0200

    update link sequence bounds and target URL length constraint

---
+++ TinyLink.Api/Migrations/20260807130452_AddLinkCodeSequence.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TinyLink.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLinkCodeSequence : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterSequence(
+                name: "link_code_req",
+                minValue: 1L,
+                maxValue: 3521614606207L,
+                oldMinValue: 1L,
+                oldMaxValue: 56L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterSequence(
+                name: "link_code_req",
+                minValue: 1L,
+                maxValue: 56L,
+                oldMinValue: 1L,
+                oldMaxValue: 3521614606207L);
+        }
+    }
+}
---
+++ TinyLink.Api/Migrations/20260807131209_AlignTargetUrlWithUrlPolicy.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TinyLink.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlignTargetUrlWithUrlPolicy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TargetUrl",
+                table: "Links",
+                type: "character varying(2000)",
+                maxLength: 2000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(2048)",
+                oldMaxLength: 2048);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TargetUrl",
+                table: "Links",
+                type: "character varying(2048)",
+                maxLength: 2048,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(2000)",
+                oldMaxLength: 2000);
+        }
+    }
+}
