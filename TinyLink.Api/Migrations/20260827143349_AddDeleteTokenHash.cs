4c62060301162f6527f49357c9d049d0011cef4e
tbyalyk <byalykt@hotmail.com>
2026-08-27 16:35:07 +0200

    feat: add DeleteTokenHash to a Link model

---
+++ TinyLink.Api/Migrations/20260827143349_AddDeleteTokenHash.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TinyLink.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeleteTokenHash : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "DeleteTokenHash",
+                table: "Links",
+                type: "bytea",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeleteTokenHash",
+                table: "Links");
+        }
+    }
+}
--- TinyLink.Api/Models/Link.cs
+++ TinyLink.Api/Models/Link.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace TinyLink.Api.Models;
 
 public class Link
@@ -5,6 +7,13 @@ public class Link
     public long Id { get; init; }
     public required string ShortCode { get; init; }
     public required Uri TargetUrl { get; init; }
+
+    [SuppressMessage(
+    "Performance",
+    "CA1819:Properties should not return arrays",
+    Justification = "EF Core maps byte arrays directly to PostgreSQL bytea columns.")]
+    public byte[]? DeleteTokenHash { get; init; }
+
     public DateTimeOffset CreatedAt { get; init; }
     public DateTimeOffset? ExpiresAt { get; init; }
     public DateTimeOffset? DeletedAt { get; set; }
